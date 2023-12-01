efce4d6efb5ce781ab1faf42c83f275dc3239613
Peregrine05 <92183530+Peregrine05@users.noreply.github.com>
2023-12-01 22:35:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# spotify-image-downloader
+
+This is a simple command-line utility written in Python for downloading most Spotify images accessible through the Spotify Web API. API access requires a Spotify application, which in turn requires a user account. Follow the [Setup](#setup) instructions below.
+
+## Setup
+
+This tool requires Python 3 (3.8+ is preferable).
+
+1. Create a virtual environment (optional).
+
+2. Run the command:
+
+```
+pip install -r requirements.txt
+```
+
+3. Package the script with [PyInstaller](https://pyinstaller.org/en/stable/) (optional).
+
+### Spotify Application Setup
+
+1. Register a Spotify account. If you are seeking to use this tool, then chances are, you already have one.
+
+2. Go to the [Spotify Developer Dashboard](https://developer.spotify.com/dashboard), and create a new app.
+
+3. Populate the `App name`, `App description`, and `Redirect URI` fields. The `Redirect URI` field may be set to `http://localhost:3000`, for example.
+
+4. Select the `Web API` to be used.
+
+5. Read and agree to the Developer Terms of Service. Save the application.
+
+6. Go to the application Settings. You need the information here for the initial use of the script.
+
+## First Use
+
+Copy the `Client ID` and paste it in place of `CLIENT_ID` in the command below. Copy the `Client secret` and paste it in place of `CLIENT_SECRET` in the command below. Then go to Spotify and copy the URL of an artist, an album, a playlist, a user, or a track. Paste it in place of `URL` in the command below.
+
+```commandLine
+python spotify-image-downloader.py URL --client-id CLIENT_ID --client-secret CLIENT_SECRET
+```
+
+After the command completes successfully, `--client-id` and `--client-secret` need not be passed in subsequent runs, unless a change is desired or necessitated.
+
+## General Usage
+
+```commandLine
+usage: Simple command-line utility to download images for Spotify users, artists, albums, and playlists.
+
+positional arguments:
+  id                    URL or URI of the Spotify resource.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OUTPUT, --output OUTPUT
+                        Output file.
+
+authorization:
+  --client-id CLIENT_ID
+                        Spotify client ID. Only required on first run.
+  --client-secret CLIENT_SECRET
+                        Spotify client secret. Only required on first run.
+  --one-time            Use the provided authorization without overwriting the saved configuration.
+  --clear               Removes the current saved Spotify client configuration.
+```
+
+Pass `--one-time` along with the Spotify application information to use the provided information without overwriting the configuration saved in `'config'`.
+
+Pass `--clear` to delete the saved configuration from `'config'` and then exit.
+
+### Examples
+
+**Downloading an image from a Spotify URL:**
+
+```commandLine
+python spotify-image-downloader.py "https://open.spotify.com/artist/0CGpNBihXlpMsjwHjVKNIO"
+```
+
+**Downloading an image from a Spotify URI:**
+
+```commandLine
+python spotify-image-downloader.py "spotify:artist:0CGpNBihXlpMsjwHjVKNIO"
+```
+
+**Specifying a custom output file:**
+
+```commandLine
+python spotify-image-downloader.py "spotify:artist:0CGpNBihXlpMsjwHjVKNIO" --output "./images/artist.jpg"
+```
---
+++ spotify-image-downloader.py
@@ -0,0 +1,172 @@
+import argparse
+import dotenv
+import os
+
+import requests
+import spotipy
+
+
+CLIENT_ID_ENV = "SPOTIPY_CLIENT_ID"
+CLIENT_SECRET_ENV = "SPOTIPY_CLIENT_SECRET"
+
+
+def parse_id(resource):
+    if "/" in resource:
+        # Spotify URL
+
+        if "spotify.com/" not in resource:
+            raise ValueError("The provided URL is not a valid Spotify URL.")
+        
+        type_and_id = resource.partition("spotify.com/")[2].partition("/")
+
+        resource_type = type_and_id[0]
+        resource_id = type_and_id[2].partition("?")[0]
+
+    elif ":" in resource:
+        # Spotify URI
+
+        if "spotify:" not in resource:
+            raise ValueError("The provided URI is not a valid Spotify URI.")
+        
+        split = resource.split(":")
+
+        resource_type = split[1]
+        resource_id = split[2]
+
+    else:
+        raise ValueError("The provided input does not resolve to a valid "
+                         "Spotify resource.")
+
+    return resource_type, resource_id
+
+
+def get_images(
+        resource_type,
+        resource_id,
+        images,
+        spotify_client_id,
+        spotify_client_secret
+):
+    client = spotipy.Spotify(
+        auth_manager=spotipy.oauth2.SpotifyClientCredentials(
+            client_id=spotify_client_id,
+            client_secret=spotify_client_secret
+        )
+    )
+
+    if resource_type == "album":
+        resource = client.album(resource_id)
+    elif resource_type == "artist":
+        resource = client.artist(resource_id)
+    elif resource_type == "playlist":
+        resource = client.playlist(resource_id)
+    elif resource_type == "track":
+        resource = client.track(resource_id)["album"]
+    elif resource_type == "user":
+        resource = client.user(resource_id)
+
+    images.extend(
+        sorted(
+            resource["images"],
+            key=(lambda x:
+                int(x["width"]) if x["width"] is not None
+                else 0 * int(x["height"]) if x["height"] is not None else 0),
+            reverse=True
+        )
+    )
+
+
+def main():
+    dotenv.load_dotenv(dotenv_path="config")
+
+    spotify_client_id = os.getenv(CLIENT_ID_ENV)
+    spotify_client_secret = os.getenv(CLIENT_SECRET_ENV)
+
+    parser = argparse.ArgumentParser(
+        prog="spotify-image-downloader",
+        usage="Simple command-line utility to download images for Spotify "
+              "users, artists, albums, and playlists."
+    )
+    parser.add_argument(
+        "id",
+        type=str,
+        help="URL or URI of the Spotify resource."
+    )
+    parser.add_argument(
+        "-o", "--output",
+        type=str,
+        required=False,
+        help="Output file."
+    )
+    authorization = parser.add_argument_group("authorization")
+    authorization.add_argument(
+        "--client-id",
+        required=spotify_client_id is None,
+        help="Spotify client ID. Only required on first run."
+    )
+    authorization.add_argument(
+        "--client-secret",
+        required=spotify_client_secret is None,
+        help="Spotify client secret. Only required on first run."
+    )
+    authorization.add_argument(
+        "--one-time",
+        action="store_true",
+        help="Use the provided authorization without overwriting the saved "
+        "configuration."
+    )
+    authorization.add_argument(
+        "--clear",
+        action="store_true",
+        help="Removes the current saved Spotify client configuration."
+    )
+
+    args = parser.parse_args()
+
+    if args.clear:
+        with open("config", "wt") as config_file:
+            config_file.write(f'{CLIENT_ID_ENV} = \n{CLIENT_SECRET_ENV}: \n')
+        return None
+
+    is_new_config = (args.client_id is not None
+                     or args.client_secret is not None)
+    if is_new_config:
+        if args.client_id is not None:
+            spotify_client_id = args.client_id
+        if args.client_secret is not None:
+            spotify_client_secret = args.client_secret
+
+    resource_type, resource_id = parse_id(args.id)
+    images = []
+    get_images(resource_type,
+               resource_id,
+               images,
+               spotify_client_id,
+               spotify_client_secret)
+    largest_image = images[0]
+    
+    if args.output is not None:
+        directory, file_name = os.path.split(args.output)
+        if len(directory):
+            try:
+                os.makedirs(directory)
+                os.chdir(directory)
+            except FileExistsError:
+                pass
+    else:
+        file_name = largest_image["url"].split("/")[-1] + ".jpg"
+
+    image = requests.get(largest_image["url"])
+    with open(file_name, "wb") as out_file:
+        out_file.write(image.content)
+    
+    if is_new_config and not args.one_time:
+        with open("config", "wt") as config_file:
+            config_file.write(
+                f"{CLIENT_ID_ENV} = {spotify_client_id}\n"
+                f"{CLIENT_SECRET_ENV} = {spotify_client_secret}\n"
+            )
+
+
+if __name__ == "__main__":
+    main()
